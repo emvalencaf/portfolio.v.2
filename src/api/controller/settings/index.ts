28acd0b8e6af8939decf22b728c8aa5d0d58977f
emvalencaf <edsonmvf@gmail.com>
2023-02-15 17:47:03 -0300

    created validation for create method of the Settings Controllers

--- src/api/controller/settings/index.ts
+++ src/api/controller/settings/index.ts
@@ -9,10 +9,33 @@ export type SettingsControllerCreate = {
 	},
 	successMessage: SuccessState;
 }
+type SettingsData = {
+	websiteName: string;
+	logo: {
+		srcImg: string;
+		altText: string;
+		link: string;
+		newTab?: boolean;
+	},
+	socialMedia?: {
+		instaURL?: string;
+		linkedinURL?: string;
+		facebookURL?: string;
+		homepageURL?: string;
+		twitterURL?: string;
+		githubURL?: string;
+		youtubeURL?: string;
+		tiktokURL?: string;
+	},
+}
+
 
 export default class SettingsController {
-	static async create(data, formData: FormData, token: string){
+	static async create(data: SettingsData, formData: FormData, token: string){
 		try{
+			
+			if (!data.logo || !data.logo?.altText || !data.logo?.srcImg || !data.logo?.link ) throw new Error(`you fill the logo fields incorrectly`);
+
 			return await SettingsService.create(formData, token);
 		} catch (err) {
 			throw new Error(err.message);
